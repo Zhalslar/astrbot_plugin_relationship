e32a32347b10fa5d12216208867a8af65b7c7bee
Zhalslar <zsl2936169201@gmail.com>
2025-04-17 20:13:28 +0800

    完善功能

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
+
+<div align="center">
+
+![:name](https://count.getloli.com/@astrbot_plugin_relationship?name=astrbot_plugin_relationship&theme=minecraft&padding=6&offset=0&align=top&scale=1&pixelated=1&darkmode=auto)
+
 # astrbot_plugin_relationship
 
+_✨ [astrbot](https://github.com/AstrBotDevs/AstrBot) 人际关系管理器 ✨_  
+
 [![License](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
-[![Python 3.8+](https://img.shields.io/badge/Python-3.8%2B-blue.svg)](https://www.python.org/)
+[![Python 3.10+](https://img.shields.io/badge/Python-3.10%2B-blue.svg)](https://www.python.org/)
 [![AstrBot](https://img.shields.io/badge/AstrBot-3.4%2B-orange.svg)](https://github.com/Soulter/AstrBot)
+[![GitHub](https://img.shields.io/badge/作者-Zhalslar-blue)](https://github.com/Zhalslar)
 
-查看QQ加的所有好友、群聊的信息
+</div>
 
-[![USER](https://img.shields.io/badge/user-Meguminlove-blue)](https://github.com/Meguminlove)
+## 💡 介绍
 
-
+为astrbot提供的一个人际关系管理器！功能包括查看好友列表、查看群列表、审批好友申请、审批群邀请、删好友、退群，方便用户管理bot的人际关系。
 
 ## 📦 安装
+
+- 可以直接在astrbot的插件市场搜索astrbot_plugin_QQProfile，点击安装即可  
+
+- 或者可以直接克隆源码到插件文件夹：
 
 ```bash
 # 克隆仓库到插件目录
@@ -20,20 +32,38 @@ git clone https://github.com/Zhalslar/astrbot_plugin_relationship
 # 控制台重启AstrBot
 ```
 
-## ⌨️ 使用命令
+## ⚙️ 配置
 
-### 基础命令
-```plaintext
-/好友列表  
-/群列表
-```
+请在astrbot面板配置，插件管理 -> astrbot_plugin_memelite -> 操作 -> 插件配置
+
+## ⌨️ 使用说明
+
+## ⌨️ 指令表
+
+|     指令      |                    说明                    |
+|:-------------:|:-----------------------------------------------:|
+| /好友列表       | 查看所有能触发meme合成的关键词  |
+| /群列表  | 具体查看某个meme的参数         |
+| (引用申请)/同意     |   同意好友申请、群邀请      |
+| (引用申请)/拒绝     |   拒绝好友申请、群邀请      |
+| /删除好友 (Q号或者@) |   删除此好友              |
+| /退群 (群号)         |   退出此群聊              |
+
+## 示例图
+
+## 🤝 TODO
+
+- [x] 查看好友列表、群列表
+- [ ] 审批好友申请，群邀请
+- [x] 删除好友、退群
+
+## 👥 贡献指南
+
+- 🌟 Star 这个项目！（点右上角的星星，感谢支持！）
+- 🐛 提交 Issue 报告问题
+- 💡 提出新功能建议
+- 🔧 提交 Pull Request 改进代码
+
 ## 📌 注意事项
-1. 建议在artrbot配置->消息平台->转发消息的字数阈值(forward_threshold) 设置低一些，防止本插件刷屏，推荐设为150
 
-
-## 🤝 参与贡献
-1. Zhalslar（饰乐），我自己哈哈哈
-
-
-## 📜 开源协议
-本项目采用 [MIT License](LICENSE)
+- 想第一时间得到反馈的可以来作者的插件反馈群（QQ群）：460973561
--- main.py
+++ main.py
@@ -1,32 +1,80 @@
-
 from astrbot.api.event import filter
+import astrbot.api.message_components as Comp
 from astrbot.api.star import Context, Star, register
-from astrbot.core.platform import AstrMessageEvent
-from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
+from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+    AiocqhttpMessageEvent,
+)
+from astrbot.core.star.filter.permission import PermissionType
 
-@register("人际关系查看器", "Zhalslar", "查看QQ加的所有好友、群聊的信息", "1.0.0", "https://github.com/Zhalslar/astrbot_plugin_relationship")
+
+@register(
+    "人际关系查看器",
+    "Zhalslar",
+    "[仅aiocqhttp] bot人际关系管理器！包括查看好友列表、查看群列表、审批好友申请、审批群邀请、删好友、退群",
+    "1.0.0",
+    "https://github.com/Zhalslar/astrbot_plugin_relationship",
+)
 class Relationship(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
+    @filter.permission_type(PermissionType.ADMIN)
     @filter.command("群列表")
-    async def show_groups_info(self, event: AstrMessageEvent):
+    async def show_groups_info(self, event: AiocqhttpMessageEvent):
         """查看加入的所有群聊信息"""
-        assert isinstance(event, AiocqhttpMessageEvent)
         client = event.bot
-        group_list =  await client.api.call_action('get_group_list')
-        group_info = "\n".join(f"{g['group_id']}: {g['group_name']}" for g in group_list)
-        yield event.plain_result(f"【群列表】共加入{len(group_list)}个群：\n{group_info}")
+        group_list = await client.get_group_list()
+        group_info = "\n".join(
+            f"{g['group_id']}: {g['group_name']}" for g in group_list
+        )
+        info = f"【群列表】共加入{len(group_list)}个群：\n{group_info}"
+        yield event.plain_result(info)
 
+    @filter.permission_type(PermissionType.ADMIN)
     @filter.command("好友列表")
-    async def show_friends_info(self, event: AstrMessageEvent):
+    async def show_friends_info(self, event: AiocqhttpMessageEvent):
         """查看所有好友信息"""
-        assert isinstance(event, AiocqhttpMessageEvent)
         client = event.bot
-        friend_list = await client.api.call_action('bot.get_friend_list')
+        friend_list = await client.get_friend_list()
         friend_info = "\n".join(f"{f['user_id']}: {f['nickname']}" for f in friend_list)
-        yield event.plain_result(f"【好友列表】共{len(friend_list)}位好友：\n{friend_info}")
+        info = f"【好友列表】共{len(friend_list)}位好友：\n{friend_info}"
+        yield event.plain_result(info)
 
+    @filter.permission_type(PermissionType.ADMIN)
+    @filter.command("退群")
+    async def set_group_leave(self, event: AiocqhttpMessageEvent, group_id: int|None = None):
+        """查看所有好友信息"""
+        if not group_id:
+            yield event.plain_result("要指明退哪个群哟~")
+            return
 
+        client = event.bot
+        group_list = await client.get_group_list()
 
+        group_ids = [int(group["group_id"]) for group in group_list]
+        if group_id not in group_ids:
+            yield event.plain_result("我没加有这个群")
+            return
 
+        await client.set_group_leave(group_id=group_id)
+        yield event.plain_result(f"已退出群聊：{group_id}")
+
+    @filter.permission_type(PermissionType.ADMIN)
+    @filter.command("删了", alias={"删除好友"})
+    async def delete_friend(self, event: AiocqhttpMessageEvent, input_id: int|None = None):
+        """查看所有好友信息"""
+        chain = event.get_messages()
+        target_id: int = input_id or next(
+            int(seg.qq) for seg in chain if (isinstance(seg, Comp.At))
+        )
+
+        client = event.bot
+        friend_list = await client.get_friend_list()
+
+        friend_ids = [int(friend["user_id"]) for friend in friend_list]
+        if target_id not in friend_ids:
+            yield event.plain_result("我没加有这个人")
+            return
+
+        await client.delete_friend(group_id=target_id)
+        yield event.plain_result(f"已删除好友：{target_id}")
