9b395ed78b646bf9c60e5ae67b9d3d47a2ee9066
Zhalslar <zsl2936169201@gmail.com>
2025-05-08 22:09:09 +0800

    修复一系列bug

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
 from astrbot.core.star.filter.permission import PermissionType
 from astrbot.core.star.filter.platform_adapter_type import PlatformAdapterType
 
+
 @register(
     "astrbot_plugin_relationship",
     "Zhalslar",
     "[仅aiocqhttp] bot人际关系管理器！包括查看好友列表、查看群列表、审批好友申请、审批群邀请、删好友、退群",
-    "1.0.3",
+    "1.0.5",
     "https://github.com/Zhalslar/astrbot_plugin_relationship",
 )
 class Relationship(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
         self.manage_group: int = config.get("manage_group", 0)
-        self.admins_id =  AstrBotConfig().get("admins_id")
-
+        self.admins_id = context.get_config().get("admins_id")
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("群列表")
@@ -29,11 +29,13 @@ class Relationship(Star):
         """查看加入的所有群聊信息"""
         client = event.bot
         group_list = await client.get_group_list()
-        group_info = "\n".join(
-            f"{g['group_id']}: {g['group_name']}" for g in group_list
+        group_info = "\n\n".join(
+            f"{i + 1}. {g['group_id']}: {g['group_name']}"
+            for i, g in enumerate(group_list)
         )
-        info = f"【群列表】共加入{len(group_list)}个群：\n{group_info}"
-        yield event.plain_result(info)
+        info = f"【群列表】共加入{len(group_list)}个群：\n\n{group_info}"
+        url = await self.text_to_image(info)
+        yield event.image_result(url)
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("好友列表")
@@ -41,9 +43,13 @@ class Relationship(Star):
         """查看所有好友信息"""
         client = event.bot
         friend_list = await client.get_friend_list()
-        friend_info = "\n".join(f"{f['user_id']}: {f['nickname']}" for f in friend_list)
-        info = f"【好友列表】共{len(friend_list)}位好友：\n{friend_info}"
-        yield event.plain_result(info)
+        friend_info = "\n\n".join(
+            f"{i + 1}. {f['user_id']}: {f['nickname']}"
+            for i, f in enumerate(friend_list)
+        )
+        info = f"【好友列表】共{len(friend_list)}位好友：\n\n{friend_info}"
+        url = await self.text_to_image(info)
+        yield event.image_result(url)
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("退群")
@@ -88,33 +94,27 @@ class Relationship(Star):
         await client.delete_friend(group_id=target_id)
         yield event.plain_result(f"已删除好友：{target_id}")
 
-
     @filter.platform_adapter_type(PlatformAdapterType.AIOCQHTTP)
     async def event_monitoring(self, event: AiocqhttpMessageEvent):
         """监听好友申请或群邀请"""
-        client = event.bot
-        notice = ""
-        #print(event.message_obj)
-        if not hasattr(event, "message_obj") or not hasattr(
-            event.message_obj, "raw_message"
+        raw_message = getattr(event.message_obj, "raw_message", None)
+
+        if (
+            not isinstance(raw_message, dict)
+            or raw_message.get("post_type") != "request"
         ):
             return
-        raw_message = event.message_obj.raw_message
-        # 处理 raw_message
-        if not raw_message or not isinstance(raw_message, dict):
-            return
-        # 确保是 request 类型的消息
-        if raw_message.get("post_type") != "request":
-            return
-
+        logger.info(f"收到好友申请或群邀请: {raw_message}")
+        client = event.bot
+        user_id: int = raw_message.get("user_id", 0)
+        nickname: str = (await client.get_stranger_info(user_id=int(user_id)))[
+            "nickname"
+        ] or "未知昵称"
+        comment: str = raw_message.get("comment") or "无"
+        flag = raw_message.get("flag")
+        notice = ""
         # 加好友事件
         if raw_message.get("request_type") == "friend":
-            user_id = raw_message.get("user_id", "")
-            comment = raw_message.get("comment") or "无"
-            flag = raw_message.get("flag")
-            nickname = (await client.get_stranger_info(user_id=int(user_id)))["nickname"] or "未知昵称"
-
-            # 通知信息
             notice = (
                 f"【收到好友申请】同意吗："
                 f"\n昵称：{nickname}"
@@ -124,14 +124,14 @@ class Relationship(Star):
             )
 
         # 群邀请事件
-        elif raw_message.get("request_type") == "group" and raw_message.get("sub_type") == "invite" :
-            user_id = raw_message.get("user_id", "")
+        elif (
+            raw_message.get("request_type") == "group"
+            and raw_message.get("sub_type") == "invite"
+        ):
             group_id = raw_message.get("group_id", "")
-            comment = raw_message.get("comment") or "无"
-            flag = raw_message.get("flag")
-            nickname = (await client.get_stranger_info(user_id=int(user_id)))["nickname"] or "未知昵称"
-            group_name = (await client.get_group_info(group_id=group_id))["group_name"] or "未知群名"
-
+            group_name = (await client.get_group_info(group_id=group_id))[
+                "group_name"
+            ] or "未知群名"
             # 通知信息
             notice = (
                 f"【收到群邀请】同意吗："
@@ -142,51 +142,45 @@ class Relationship(Star):
                 f"\nflag：{flag}"
                 f"\n验证信息：{comment}"
             )
-            group_list = await client.get_group_list()
-            gids = [str(f["group_id"]) for f in group_list]
-            if str(user_id) in gids:
-                yield event.plain_result(f"想要我进群？先进管理群：{self.manage_group}")
 
-        # 反馈管理员（群）
+        if not notice:
+            return
+
+        # 反馈给管理群
         if self.manage_group:
             await client.send_group_msg(group_id=self.manage_group, message=notice)
+        # 反馈给管理员
         elif self.admins_id:
-            for admin_id in self.admins_id:
+            admins_id = [id for id in self.admins_id if id.isdigit()]
+            for admin_id in admins_id:
                 await client.send_private_msg(user_id=int(admin_id), message=notice)
 
-        # 反馈用户
-        friend_list = await client.get_friend_list()
-        uids = [str(f["user_id"]) for f in friend_list]
-        if str(user_id) in uids:
+        # 反馈给用户
+        friend_list: list[dict] = await client.get_friend_list() # type: ignore
+        friend_ids: list[int] = [f["user_id"] for f in friend_list]
+        if user_id in friend_ids:
+            reply = (
+                f"想加好友或拉群？要等开发群{self.manage_group}审批哟"
+                if self.manage_group
+                else "想加好友或拉群？要等Bot管理员审批哟"
+            )
             try:
-                if self.manage_group:
-                    yield event.plain_result(f"想加好友或拉群？要等开发群{self.manage_group}审批哟")
-                else:
-                    yield event.plain_result("想加好友或拉群？要等bot管理员审批哟")
+                await client.send_private_msg(user_id=int(user_id), message=reply)
             except Exception as e:
                 logger.error(f"无法反馈用户：{e}")
-
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("同意")
     async def agree(self, event: AiocqhttpMessageEvent, extra: str = ""):
         """同意好友申请或群邀请"""
-        reply = await self.approve(
-            event=event,
-            extra=extra,
-            approve=True
-        )
+        reply = await self.approve(event=event, extra=extra, approve=True)
         yield event.plain_result(reply)
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("拒绝")
     async def refuse(self, event: AiocqhttpMessageEvent, extra: str = ""):
         """拒绝好友申请或群邀请"""
-        reply = await self.approve(
-            event=event,
-            extra=extra,
-            approve=False
-        )
+        reply = await self.approve(event=event, extra=extra, approve=False)
         if reply:
             yield event.plain_result(reply)
 
@@ -203,7 +197,7 @@ class Relationship(Star):
                 if isinstance(seg, Comp.Plain):
                     text = seg.text
         lines = text.split("\n")
-        client=event.bot
+        client = event.bot
         reply = ""
         if "【收到好友申请】" in text and len(lines) >= 5:
             nickname = lines[1].split("：")[1]  # 第2行冒号后文本为nickname
@@ -250,4 +244,3 @@ class Relationship(Star):
                     reply = "这条申请处理过了或者格式不对"
 
         return reply
-
